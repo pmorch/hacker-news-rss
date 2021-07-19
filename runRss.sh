6a25fc991d9f1b101985edbbe57fa7e7789830e8
Peter Valdemar Mørch <peter@morch.com>
2021-07-19 11:58:17 +0200

    Put perl output in output/hackerNews100-pl.xml

--- runRss.sh
+++ runRss.sh
@@ -3,8 +3,12 @@ set -e
 cd $(dirname $0)
 mkdir -p output
 logfile=output/run.log
+
 outfile=output/hackerNews100.xml
 echo '*******************************' >> $logfile
 date >> $logfile
-./rss.pl > $outfile 2>>$logfile
+./rss.js > $outfile 2>>$logfile
 
+outfilePL=output/hackerNews100-pl.xml
+echo '-------------------------------' >> $logfile
+./rss.pl > $outfilePL 2>>$logfile
